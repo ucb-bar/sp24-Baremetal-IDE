dee0247b54d2f483a0a5984a407d02c7847aef9a
Ted Kim <tedkim7@bwrc-lab04.EECS.Berkeley.EDU>
2025-01-21 19:01:12 -0800

    Finish shmoo plot

--- platform/dsp24/freq.h
+++ platform/dsp24/freq.h
@@ -1,5 +1,5 @@
 #ifndef __FREQ_H
                          #define __FREQ_H
-                         #define MTIME_FREQ     1400000000
-                         #define SYS_CLK_FREQ   1400000000
+                         #define MTIME_FREQ     1050000000
+                         #define SYS_CLK_FREQ   1050000000
                          #endif
