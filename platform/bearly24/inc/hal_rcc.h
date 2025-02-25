8f2379da95d7f631c89f65e55fe46fdbcaae89fd
Jasmine Angle <12690737+jasmangle@users.noreply.github.com>
2025-02-25 06:16:45 +0000

    Initial PRCI drivers for Bearly and DSP

---
+++ platform/bearly24/inc/hal_rcc.h
@@ -0,0 +1,26 @@
+/**
+ * @file hal_rcc.h
+ * @author Jasmine Angle (angle@berkeley.edu)
+ * @brief 
+ * @version 0.1
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+ #ifndef __HAL_RCC_H
+ #define __HAL_RCC_H
+ 
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+ 
+ #include "metal.h"
+ #include "ll_rcc.h"
+ 
+ #ifdef __cplusplus
+ }
+ #endif
+ 
+ #endif /* __HAL_RCC_H */
+ 
