bbbbf4cfff410cb9a06181cb1a1f2ae53e0e6348
lucy <srevi@berkeley.edu>
2025-03-14 21:54:38 -0700

    reverting pointer change despite error since maybe intentional

--- platform/dsp24/src/hal_DMA.c
+++ platform/dsp24/src/hal_DMA.c
@@ -7,7 +7,6 @@ int DMA_count(int num) {
     return reg_read32(DMA_COUNT + num*DMA_MMIO_Offset);
 }
 
-
 void set_DMAC(uint32_t num, uint64_t addr_r, uint64_t addr_w, uint16_t inc_r, uint16_t inc_w, uint16_t len, uint16_t logd) {
     reg_write64(DMA_SET_READ + num*DMA_MMIO_Offset, addr_r);
     reg_write64(DMA_SET_WRITE + num*DMA_MMIO_Offset, addr_w);
@@ -19,11 +18,10 @@ void set_DMAC(uint32_t num, uint64_t addr_r, uint64_t addr_w, uint16_t inc_r, ui
     reg_write8(DMA_SET_LOGD + num*DMA_MMIO_Offset, logd);
 }
 
-
 void set_DMAP(uint32_t num, uint64_t* addr_r, uint64_t* addr_w, uint64_t* addr_c, uint16_t inc_r, uint16_t inc_w, uint16_t len, uint8_t logd, uint32_t count) {
-    reg_write64(DMA_SET_READ + num*DMA_MMIO_Offset, *addr_r);
-    reg_write64(DMA_SET_WRITE + num*DMA_MMIO_Offset, *addr_w);
-    reg_write64(DMA_SET_C + num*DMA_MMIO_Offset, *addr_c);
+    reg_write64(DMA_SET_READ + num*DMA_MMIO_Offset, addr_r);
+    reg_write64(DMA_SET_WRITE + num*DMA_MMIO_Offset, addr_w);
+    reg_write64(DMA_SET_C + num*DMA_MMIO_Offset, addr_c);
 
     reg_write16(DMA_INC_READ + num*DMA_MMIO_Offset, inc_r);
     reg_write16(DMA_INC_WRITE + num*DMA_MMIO_Offset, inc_w);
