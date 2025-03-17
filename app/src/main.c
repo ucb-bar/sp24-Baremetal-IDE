4ad6ce8c9584fee442f25f31bfd2435b276e4c19
Tushar Goyal <tushar.goyal@bwrcrdsl-5.EECS.Berkeley.EDU>
2025-03-17 11:52:53 -0700

    added igemm without caching

--- app/src/main.c
+++ app/src/main.c
@@ -331,6 +331,57 @@ void benchmark_vec_iconv(){
   }
 }
 
+extern uint64_t M;
+extern uint64_t N;
+extern uint64_t P;
+
+extern int64_t a[] __attribute__((aligned(256)));
+extern int64_t b[] __attribute__((aligned(256)));
+extern int64_t c[] __attribute__((aligned(256)));
+// Gold results
+extern int64_t g[] __attribute__((aligned(256)));
+
+void benchmark_vec_igemm() { 
+  printf("IMATMUL\n");
+  unsigned long cycles1, cycles2, instr2, instr1;
+
+  for (int s = 4; s <= M; s *= 2) {
+    printf("Calculating a (%d x %d) x (%d x %d) matrix multiplication...\n", s,
+           s, s, s);
+
+    // Matrices are initialized --> Start calculating
+    printf("Calculating imatmul...\n");
+    instr1 =  READ_CSR("minstret");
+    cycles1 = READ_CSR("mcycle");
+    imatmul(c, a, b, s, s, s);
+    asm volatile("fence");
+    instr2 =  READ_CSR("minstret");
+    cycles2 = READ_CSR("mcycle");
+
+    // Metrics
+    int64_t runtime = cycles2 - cycles1;
+    float performance = 2.0 * s * s * s / runtime;
+
+    printf("The execution took %d cycles.\n", runtime);
+    printf("The performance is %ld OPs/1000 cycles.\n", (uint64_t)(1000.0 * performance));
+
+    // Verify the result only for s == M (to keep it simple)
+    if (s == M) {
+      // Verify the result
+      printf("Verifying result...\n");
+      int error = verify_matrix(c, g, s, s);
+      if (error != 0) {
+        printf("Error code %d\n", error);
+        printf("c[%d]=%d\n", error, c[error]);
+      } else {
+        printf("Passed.\n");
+      }
+    }
+  }
+}
+
+
+
 void app_init() {
   // torch::executor::runtime_init();
 }
@@ -370,6 +421,7 @@ int main(int argc, char **argv) {
 
   // benchmark_vec_dot_product();
   benchmark_vec_iconv();
+  // benchmark_vec_igemm();
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
