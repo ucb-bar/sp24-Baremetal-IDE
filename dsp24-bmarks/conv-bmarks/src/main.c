a1122596e0e2af999e532bcf3c22d8fcadca3cb6
jel221 <jlaurenceleigh@gmail.com>
2025-04-01 20:45:45 -0700

    Conv

--- dsp24-bmarks/conv-bmarks/src/main.c
+++ dsp24-bmarks/conv-bmarks/src/main.c
@@ -114,6 +114,104 @@ void convaccel_test(int seed) {
 
 }
 
+
+void conv_acc(uint8_t *input_audio, size_t audio_len, uint16_t *input_kernel, size_t kernel_len, size_t dilation, uint16_t *output_audio);
+
+void conv_test() {
+  size_t audio_len = story_bearly_wav_len;
+  size_t kernel_len = 8;
+  size_t dilation = 1;
+
+  uint8_t* input_audio_ = audio;
+  for (int i = 0; i < audio_len; i++) {
+    input_audio_[i] = input_audio_[i] / 2;
+  }
+
+  uint16_t output_audio_cpu[16];
+  uint16_t output_audio_acc[16];
+  uint16_t input_kernel[8] = {0x3000, 0x3000, 0x3000, 0x3000, 0x3000, 0x3000, 0x3000, 0x3000};
+
+  uint8_t* input_audio;
+  for (int stride = 0; stride < audio_len; stride += 16) {
+    input_audio = input_audio_ + stride;
+
+    software_conv(input_audio, 16, input_kernel, kernel_len, dilation, output_audio_cpu);
+    conv_acc(input_audio, 16, input_kernel, kernel_len, dilation, output_audio_acc);
+
+  // for(int i = 0; i < audio_len; i++) {
+  //   if(output_audio_cpu[i] != output_audio_acc[i]) {
+  //     printf("\r\nmismatch at index %d\r\n", i);
+  //   }
+  // }
+
+  // printf("\r\nFinished");
+  // printf("\r\noutput_audio_acc:\r\n");
+  // for(int i = 0; i < audio_len; i++) {
+  //   printf("0x%x - %d\r\n", output_audio_acc[i], f16_int(output_audio_acc[i]));
+  // }
+    for(int i = 0; i < 16; i++) {
+        printf("%d ", f16_int(output_audio_acc[i]));
+    }
+    printf("\r\n");
+  }
+}
+
+void conv_acc(uint8_t *input_audio, size_t audio_len, uint16_t *input_kernel, size_t kernel_len, size_t dilation, uint16_t *output_audio) {
+  reg_write64(CONV_BASE, *((uint64_t*) (input_audio)));
+  reg_write64(CONV_BASE, *((uint64_t*) (input_audio + 8)));
+  set_conv_params(audio_len >= 16 ? 16 : audio_len, dilation, input_kernel);
+  start_conv();
+  asm volatile("fence");
+
+  for (int i = 0; i < 4; i++) {
+    uint64_t current_out = reg_read64(CONV_OUTPUT_ADDR);
+    uint16_t* unpacked_out = (uint16_t*) &current_out;
+    for (int j = 0; j < 4; j++) {
+      output_audio[i*4 + j] = unpacked_out[j];
+    }
+  }
+
+  reg_write8(CONV_START_ADDR, 0);
+  reg_write8(RESET_ADDR, 1);
+  asm volatile("fence");
+
+  uint64_t start_index = 1;
+  for(uint64_t i = 9; i < audio_len - 2 * kernel_len; i++) {
+
+    uint8_t input_audio_temp[16];
+    for(int j = 0; j < 16; j++) {
+      input_audio_temp[j] = input_audio[start_index + j];
+    }
+
+    reg_write64(CONV_BASE, *((uint64_t*) (input_audio_temp)));
+    reg_write64(CONV_BASE, *((uint64_t*) (input_audio_temp + 8)));
+    set_conv_params(16, dilation, input_kernel);
+    start_conv();
+    asm volatile("fence");
+
+    // uint64_t current_out = reg_read64(CONV_OUTPUT_ADDR);
+    // uint16_t* unpacked_out = (uint16_t*) &current_out;
+    // output_audio[i] = unpacked_out[8];
+
+    uint16_t output_audio_temp[16];
+    for (int x = 0; x < 4; x++) {
+      uint64_t current_out = reg_read64(CONV_OUTPUT_ADDR);
+      uint16_t* unpacked_out = (uint16_t*) &current_out;
+      for (int y = 0; y < 4; y++) {
+        output_audio_temp[x*4 + y] = unpacked_out[y];
+      }
+    }
+
+    output_audio[i] = output_audio_temp[8];
+      
+    reg_write8(CONV_START_ADDR, 0);
+    reg_write8(RESET_ADDR, 1);
+    asm volatile("fence");
+
+    start_index += 1;
+  }
+}
+
 /**
   * @brief  The application entry point.
   * @retval int
