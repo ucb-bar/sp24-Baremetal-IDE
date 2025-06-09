8048a520868d4329849cdf6cff50b25fe1c531af
lucy <srevi@berkeley.edu>
2025-06-08 20:58:05 -0700

    draft CLI controller

--- app/src/main.c
+++ app/src/main.c
@@ -19,10 +19,172 @@
 #include "chip_config.h"
 #include "bmi088.h"
 #include <math.h>
+#include <time.h>
 
 int motor_speeds[8];
 int motor_positions[8];
 
+/* USER CODE BEGIN PV */
+const unsigned char *ASCII_CRLF = (const unsigned char *) "\r\n";
+const unsigned char *ASCII_BEL = (const unsigned char *) "\a";
+
+void read_stdin(const char* guide, char* buffer, size_t bufsize) {
+  // read a line from stdin, up to but not including \n
+  // printf("%s", guide);
+  // if (fgets(buffer, bufsize, stdin) != NULL) {
+  //     size_t len = strlen(buffer);
+  //     if (len > 0 && buffer[len - 1] == '\n') {
+  //         buffer[len - 1] = '\0'; // strip newline
+  //     }
+  // }
+  size_t char_offset = 0;
+  size_t upper_bufsize_bound = bufsize - 1;
+  printf("%s", guide);
+  fflush(stdout);
+
+  while(1) {
+    unsigned char input_char = '\0';
+    uart_receive(UART0, &input_char, 1, 100);
+
+    if (input_char == '\b' && char_offset > 0) {
+      // Backspace handling
+      uart_transmit(UART0, &input_char, 1, 100);
+      char_offset--;
+      *(buffer + char_offset) = '\0';
+    } else if (input_char == '\r') {
+      // Newline (submit) handling
+      uart_transmit(UART0, ASCII_CRLF, 2, 100);
+      *(buffer + char_offset) = '\0';
+      break;
+    } else if (input_char > 31 && input_char < 127 && char_offset < upper_bufsize_bound) {
+      // Any printable ascii character
+      uart_transmit(UART0, &input_char, 1, 100);
+      *(buffer + char_offset) = input_char;
+      char_offset++;
+    } else {
+      // Send a bell to the terminal if some other condition.
+      uart_transmit(UART0, ASCII_BEL, 1, 100);
+    }
+  }
+}
+
+#include "switch.h"
+#include "joints.h"
+#include "controller.h"
+
+#define F_LOW 530
+#define F_HIGH 1000
+
+#define F_FRONT 150
+#define F_BACK 800
+
+#define B_FRONT 800
+#define B_BACK 150
+
+// Forward gait
+int step_fwd[6][8] = {
+    {F_LOW, F_FRONT, F_HIGH, B_BACK, F_BACK, F_HIGH, B_FRONT, F_LOW},
+    {F_LOW, F_BACK, F_HIGH, B_FRONT, F_FRONT, F_HIGH, B_BACK, F_LOW},
+    {F_LOW, F_BACK, F_LOW, B_FRONT, F_FRONT, F_LOW, B_BACK, F_LOW},
+    {F_HIGH, F_BACK, F_LOW, B_FRONT, F_FRONT, F_LOW, B_BACK, F_HIGH},
+    {F_HIGH, F_FRONT, F_LOW, B_BACK, F_BACK, F_LOW, B_FRONT, F_HIGH},
+    {F_LOW, F_FRONT, F_LOW, B_BACK, F_BACK, F_LOW, B_FRONT, F_LOW},
+};
+
+// Backward gait (mirror of forward)
+int step_back[6][8] = {
+    {F_HIGH, F_BACK, F_LOW, B_FRONT, F_FRONT, F_LOW, B_BACK, F_HIGH},
+    {F_HIGH, F_FRONT, F_LOW, B_BACK, F_BACK, F_LOW, B_FRONT, F_HIGH},
+    {F_LOW, F_FRONT, F_LOW, B_BACK, F_BACK, F_LOW, B_FRONT, F_LOW},
+    {F_LOW, F_BACK, F_LOW, B_FRONT, F_FRONT, F_LOW, B_BACK, F_LOW},
+    {F_LOW, F_BACK, F_HIGH, B_FRONT, F_FRONT, F_HIGH, B_BACK, F_LOW},
+    {F_LOW, F_FRONT, F_HIGH, B_BACK, F_BACK, F_HIGH, B_FRONT, F_LOW},
+};
+
+// Left turn gait (exaggerated left side backward, right side forward)
+int turn_left_gait[6][8] = {
+    {F_HIGH, F_BACK, F_LOW, B_FRONT, F_FRONT, F_LOW, B_BACK, F_HIGH},
+    {F_HIGH, F_BACK, F_HIGH, B_BACK, F_BACK, F_HIGH, B_FRONT, F_LOW},
+    {F_LOW, F_BACK, F_LOW, B_FRONT, F_FRONT, F_LOW, B_BACK, F_LOW},
+    {F_HIGH, F_FRONT, F_LOW, B_BACK, F_BACK, F_LOW, B_FRONT, F_HIGH},
+    {F_HIGH, F_BACK, F_LOW, B_BACK, F_BACK, F_LOW, B_FRONT, F_HIGH},
+    {F_LOW, F_BACK, F_LOW, B_BACK, F_BACK, F_LOW, B_FRONT, F_LOW},
+};
+
+// Right turn gait (mirror of left)
+int turn_right_gait[6][8] = {
+    {F_LOW, F_FRONT, F_HIGH, B_BACK, F_BACK, F_HIGH, B_FRONT, F_LOW},
+    {F_LOW, F_BACK, F_HIGH, B_FRONT, F_FRONT, F_HIGH, B_BACK, F_LOW},
+    {F_LOW, F_BACK, F_LOW, B_FRONT, F_FRONT, F_LOW, B_BACK, F_LOW},
+    {F_HIGH, F_BACK, F_LOW, B_FRONT, F_FRONT, F_LOW, B_BACK, F_HIGH},
+    {F_HIGH, F_FRONT, F_LOW, B_BACK, F_BACK, F_LOW, B_FRONT, F_HIGH},
+    {F_LOW, F_FRONT, F_LOW, B_BACK, F_BACK, F_LOW, B_FRONT, F_LOW},
+};
+
+// Perform a full gait step (6-frame motion)
+void step_with_gait(int gait[6][8]) {
+    for (int t = 0; t < 6; t++) {
+        for (int i = 0; i < 8; i++) {
+            set_motor_pos(i, gait[t][i]);
+        }
+        msleep(1000);
+    }
+}
+
+// CLI command handler
+void controller_cli(char *cli_user_prompt, int steps) {
+    char user_prompt[16];
+    printf("Hi, my name is George! What shall I do now? [front, left, right, back, dance, exit]\n");
+
+    srand(time(NULL)); // Initialize RNG
+
+    while (1) {
+        // Read input from function argument (first time only)
+        if (cli_user_prompt != NULL) {
+            strncpy(user_prompt, cli_user_prompt, sizeof(user_prompt) - 1);
+            user_prompt[sizeof(user_prompt) - 1] = '\0';
+            cli_user_prompt = NULL;
+        } else {
+            read_stdin("User: ", user_prompt, sizeof(user_prompt));
+        }
+
+        if (strcmp(user_prompt, "front") == 0) {
+            printf("Stepping forward!\n");
+            for (int i = 0; i < steps; i++) step_with_gait(step_fwd);
+
+        } else if (strcmp(user_prompt, "back") == 0) {
+            printf("Stepping backward!\n");
+            for (int i = 0; i < steps; i++) step_with_gait(step_back);
+
+        } else if (strcmp(user_prompt, "left") == 0) {
+            printf("Turning left!\n");
+            for (int i = 0; i < steps; i++) step_with_gait(turn_left_gait);
+
+        } else if (strcmp(user_prompt, "right") == 0) {
+            printf("Turning right!\n");
+            for (int i = 0; i < steps; i++) step_with_gait(turn_right_gait);
+
+        } else if (strcmp(user_prompt, "dance") == 0) {
+            printf("Dance party!\n");
+            for (int i = 0; i < 5; ++i) {
+                for (int m = 0; m < 8; ++m) {
+                    int pos = (rand() % 2) ? F_HIGH : F_BACK;
+                    set_motor_pos(m, pos);
+                }
+                msleep(300);
+            }
+
+        } else if (strcmp(user_prompt, "exit") == 0 || strcmp(user_prompt, "quit") == 0) {
+            printf("Exiting...\n");
+            break;
+
+        } else {
+            printf("Unknown command: '%s'\n", user_prompt);
+        }
+    }
+
+    printf("Okay - bye bye!\n");
+}
 
 void app_init() {
 
